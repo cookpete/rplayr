240143811961b410f6fdfd0b5d426e0e9193beb8
Pete Cook <pete@cookpete.com>
2016-02-02 14:22:19 +0000

    Open FAQ links in new window

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,15 +19,15 @@ export default function About () {
         </dd>
         <dt>How does it work?</dt>
         <dd>
-          The app is written using <a href={REACT_URL}>React</a>. It fetches stuff from Reddit using <a href={FETCH_REDDIT_URL}>fetch-reddit</a>, then <a href={REACT_PLAYER_URL}>react-player</a> plays any supported URLs. See the <a href={SOURCE_URL}>source code</a> on GitHub. Playlists are curated by the mighty <a href={CURATION_URL}>u/evilnight</a> over at <a href={LISTENTOTHIS_URL}>r/listentothis</a>.
+          The app is written using <a href={REACT_URL} target='_blank'>React</a>. It fetches stuff from Reddit using <a href={FETCH_REDDIT_URL} target='_blank'>fetch-reddit</a>, then <a href={REACT_PLAYER_URL} target='_blank'>react-player</a> plays any supported URLs. See the <a href={SOURCE_URL} target='_blank'>source code</a> on GitHub. Playlists are curated by the mighty <a href={CURATION_URL} target='_blank'>u/evilnight</a> over at <a href={LISTENTOTHIS_URL} target='_blank'>r/listentothis</a>.
         </dd>
         <dt>What can it play?</dt>
         <dd>
           {APP_NAME} currently supports YouTube, SoundCloud and Vimeo links.
         </dd>
-        <dt>What about <a href='http://radd.it'>radd.it</a>, <a href='https://reddit.musicplayer.io'>reddit.musicplayer.io</a>, <a href='http://redditplaylister.phoenixforgotten.com'>redditplaylister</a>, etc?</dt>
+        <dt>What about <a href='http://radd.it' target='_blank'>radd.it</a>, <a href='https://reddit.musicplayer.io' target='_blank'>reddit.musicplayer.io</a>, <a href='http://redditplaylister.phoenixforgotten.com' target='_blank'>redditplaylister</a>, etc?</dt>
         <dd>
-          Yes, there are other apps that do the job. I wanted to create something simple with a clean, minimal interface. Also, this app fixes <a href='https://github.com/CookPete/react-player/issues/7'>issues</a> that other apps still suffer from.
+          Yes, there are other apps that do the job. I wanted to create something simple with a clean, minimal interface. Also, this app fixes <a href='https://github.com/CookPete/react-player/issues/7' target='_blank'>issues</a> that other apps still suffer from.
         </dd>
       </dl>
     </section>
