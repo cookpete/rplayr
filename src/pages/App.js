66a8c8510777597590893cd6b88c10ca648e10dd
Pete Cook <pete@cookpete.com>
2016-03-02 13:02:48 +0000

    Dynamic coloured favicons

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import randomcolor from 'randomcolor'
 
 import 'normalize.css/normalize.css'
 import '../styles/defaults.scss'
@@ -12,8 +13,28 @@ export default class App extends Component {
     children: PropTypes.node
   };
   componentDidMount () {
+    const { params, location } = this.props
+    this.updateFavicon(params, location)
     if (window.performance) {
       trackTiming('react', 'firstrender', Math.round(window.performance.now()))
+    }
+  }
+  componentWillReceiveProps ({ params, location }) {
+    this.updateFavicon(params, location.pathname)
+  }
+  updateFavicon (params, pathname) {
+    const seed = params.post_id || params.subreddit || params.multi || pathname
+    const canvas = document.createElement('canvas')
+    canvas.width = 32
+    canvas.height = 32
+    if (canvas.getContext) {
+      const ctx = canvas.getContext('2d')
+      ctx.beginPath()
+      ctx.arc(16, 16, 16, 0, 360)
+      ctx.fillStyle = randomcolor({ seed, luminosity: 'light' })
+      ctx.fill()
+      const url = canvas.toDataURL('image/png')
+      document.querySelector('link[rel="icon"]').setAttribute('href', url)
     }
   }
   render () {
